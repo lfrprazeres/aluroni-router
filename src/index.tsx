4c2dc06a47d8770d06384b0a8b4df5d5795a1535
Luiz Fernando <lfrprazeres@gmail.com>
2022-02-23 23:29:08 -0300

    1.2 - Criando o eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "normalize.css";
-import "./index.css";
-import Cardapio from "./pages/Cardapio";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css';
+import './index.css';
+import Cardapio from './pages/Cardapio';
 
 ReactDOM.render(
   <React.StrictMode>
